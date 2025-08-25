e115490976e9ad81caabf3aae79fbdb19e95fb03
MihaiNeagu9 <mihaineagu2001@yahoo.com>
2025-08-25 18:22:45 +0300

    Create columns, image and description

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Neagu Mihai-Daniel")
+    content = """ 
+    Absolvent al Facultății de Energetică, specializarea Energetică și
+Tehnologii Nucleare, cu o bază solidă de cunoștințe teoretice în
+domeniul energetic. Deși nu am experiență practică directă, sunt
+dornic să aplic conceptele învățate și să îmi dezvolt abilitățile întrun mediu profesionist. Sunt motivat să contribui la proiecte
+inovatoare și să învăț într-o echipă dinamică, orientată spre soluții
+sigure și sustenabile în sectorul energetic.
+    """
+    st.info(content)
